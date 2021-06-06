c921d4c8f613f173871f41b5d600691094cfd1db
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-06-06 12:40:24 +0200

    gitflow-hotfix-stash: all-volume-fix

--- choices/single_manga.py
+++ choices/single_manga.py
@@ -1,6 +1,7 @@
 from .single_chapter import get_single_chapter
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def get_single_manga(link, choice):
     try:
@@ -12,10 +13,17 @@ def get_single_manga(link, choice):
             new_link += '/'
         link = new_link
         soup = BeautifulSoup(requests.get(link).content, 'html.parser')
-        all_volumes = soup.find('a', class_='chap')
-        for chapter in all_volumes:
+        volume_number = str(soup.find('p', class_='volume-name d-inline'))
+        new_volume_number = [float(i) for i in re.findall(r'-?\d+\.?\d*', volume_number)]
+        new_volume_number = int(new_volume_number[0])
+        all_volumes = soup.find_all('a', class_='chap')
+        for chapter in range(int(len(all_volumes))):
             link = all_volumes['href']
             link += "/1"
+            file = open('link.txt', 'a')
+            file.write(str(new_volume_number) + "\n")
+            file.close()
+            new_volume_number = new_volume_number -1
             get_single_chapter(link, choice)
-    except:
+    except: 
         print('-errore-')
